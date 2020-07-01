9045115f7405e2797891139308389cd3e1ae1619
FlorentMeyer <67634192+FlorentMeyer@users.noreply.github.com>
2020-07-01 15:03:01 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -167,39 +167,49 @@ def ap_per_class(tp, conf, pred_cls, target_cls):
     pr_score = 0.1  # score to evaluate P and R https://github.com/ultralytics/yolov3/issues/898
     s = [unique_classes.shape[0], tp.shape[1]]  # number class, number iou thresholds (i.e. 10 for mAP0.5...0.95)
     ap, p, r = np.zeros(s), np.zeros(s), np.zeros(s)
-    for ci, c in enumerate(unique_classes):
-        i = pred_cls == c
-        n_gt = (target_cls == c).sum()  # Number of ground truth objects
-        n_p = i.sum()  # Number of predicted objects
+    
+    with open("precisionRecall.txt", "w") as f:
+        for ci, c in enumerate(unique_classes):
+            i = pred_cls == c
+            n_gt = (target_cls == c).sum()  # Number of ground truth objects
+            n_p = i.sum()  # Number of predicted objects
 
-        if n_p == 0 or n_gt == 0:
-            continue
-        else:
-            # Accumulate FPs and TPs
-            fpc = (1 - tp[i]).cumsum(0)
-            tpc = tp[i].cumsum(0)
+            if n_p == 0 or n_gt == 0:
+                continue
+            else:
+                # Accumulate FPs and TPs
+                fpc = (1 - tp[i]).cumsum(0)
+                tpc = tp[i].cumsum(0)
 
-            # Recall
-            recall = tpc / (n_gt + 1e-16)  # recall curve
-            r[ci] = np.interp(-pr_score, -conf[i], recall[:, 0])  # r at pr_score, negative x, xp because xp decreases
+                # Recall
+                recall = tpc / (n_gt + 1e-16)  # recall curve
+                r[ci] = np.interp(-pr_score, -conf[i], recall[:, 0])  # r at pr_score, negative x, xp because xp decreases
 
-            # Precision
-            precision = tpc / (tpc + fpc)  # precision curve
-            p[ci] = np.interp(-pr_score, -conf[i], precision[:, 0])  # p at pr_score
+                # Precision
+                precision = tpc / (tpc + fpc)  # precision curve
+                p[ci] = np.interp(-pr_score, -conf[i], precision[:, 0])  # p at pr_score
 
-            # AP from recall-precision curve
-            for j in range(tp.shape[1]):
-                ap[ci, j] = compute_ap(recall[:, j], precision[:, j])
+                # AP from recall-precision curve
+                for j in range(tp.shape[1]):
+                    ap[ci, j] = compute_ap(recall[:, j], precision[:, j])
 
-            # Plot
-            # fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-            # ax.plot(recall, precision)
-            # ax.set_xlabel('Recall')
-            # ax.set_ylabel('Precision')
-            # ax.set_xlim(0, 1.01)
-            # ax.set_ylim(0, 1.01)
-            # fig.tight_layout()
-            # fig.savefig('PR_curve.png', dpi=300)
+                f.write(f'{c}' + '\n')
+                for x in precision:
+                    f.write(f'{x} ')
+                f.write('\n')
+                for x in recall:
+                    f.write(f'{x} ')
+                f.write('\n')
+                
+                # Plot
+                # fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+                # ax.plot(recall, precision)
+                # ax.set_xlabel('Recall')
+                # ax.set_ylabel('Precision')
+                # ax.set_xlim(0, 1.01)
+                # ax.set_ylim(0, 1.01)
+                # fig.tight_layout()
+                # fig.savefig('PR_curve.png', dpi=300)
 
     # Compute F1 score (harmonic mean of precision and recall)
     f1 = 2 * p * r / (p + r + 1e-16)
